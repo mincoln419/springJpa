4914f2b03f783532020cc7d3f4d2678c28160889
mermer <mincoln419@naver.com>
2023-03-15 17:53:55 +0900

    Connection Pool Datasource test

--- jdbc/src/test/java/com/example/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/com/example/jdbc/connection/ConnectionTest.java
@@ -13,6 +13,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -42,10 +44,23 @@ public class ConnectionTest {
 		useDataSource(dataSource);
 	}
 	
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(1);
+		dataSource.setPoolName("MyPool");
+		useDataSource(dataSource);
+		Thread.sleep(3000);
+	}
+	
 	private void useDataSource(DataSource dataSource) throws SQLException {
 		Connection connection1 = dataSource.getConnection();
 		Connection connection2 = dataSource.getConnection();
 		log.info("connection={}, class={}", connection1, connection1.getClass());
 		log.info("connection={}, class={}", connection2, connection2.getClass());
+		connection1.close();
 	}
 }
