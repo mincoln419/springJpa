a7727be1e6722b175ebb6842ebce3231439b8644
mermer <mincoln419@naver.com>
2023-03-15 14:56:46 +0900

    init project

---
+++ springjpa/src/main/java/com/mermer/springIoc/SpringIocApplication.java
@@ -0,0 +1,13 @@
+package com.mermer.springIoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocApplication.class, args);
+	}
+
+}
